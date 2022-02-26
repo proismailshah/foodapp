72a00f653bfd0e583119b329cd16fb98308eee48
proismailshah <smily124000@gmail.com>
2022-02-27 01:09:25 +0500

    learned more about provider

--- foodapp/lib/main.dart
+++ foodapp/lib/main.dart
@@ -33,7 +33,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Provider.of<Counter>(context).count++;
+    var count = context.watch<Counter>(); // <=== WATCH
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -41,16 +41,35 @@ class Home extends StatelessWidget {
           Consumer<Counter>(
             // <=== DEPENDENT
             builder: (context, counter, child) => Text(
-              '${counter.count}',
+              '${count.count}',
               style: Theme.of(context).textTheme.bodyLarge,
             ),
           ),
           Builder(builder: (context) {
             // <=== DEPENDENT
-            final counter = Provider.of<Counter>(context, listen: false);
-            return RaisedButton(
-              onPressed: () => counter.increment(),
-              child: const Text("Increment"),
+            final counter = context.read<Counter>();
+            return Column(
+              children: [
+                RaisedButton(
+                  onPressed: () => counter.increment(),
+                  child: const Text("Next Screen"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: ((context) {
+                          Counter count = context.watch<Counter>();
+                          return ChangeNotifierProvider.value(
+                              value: count, child: const SecondScreen());
+                        }),
+                      ),
+                    );
+                  },
+                  child: const Text("Increment"),
+                ),
+              ],
             );
           }),
         ],
@@ -60,10 +79,88 @@ class Home extends StatelessWidget {
 }
 
 class Counter with ChangeNotifier {
-  int count = 0;
+  int count = 5;
 
   void increment() {
     ++count;
     notifyListeners();
   }
 }
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    print('Inistate');
+    controller = TextEditingController();
+    // func(BuildContext context) {
+    //   context.watch()<Counter>.count = 5;
+    // }
+
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant SecondScreen oldWidget) {
+    print('Update the widget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print('dispose');
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var count = context.watch<Counter>();
+    // func(context); // <=== WATCH
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.red,
+              width: 200,
+              height: 200,
+              child: Center(child: Text(count.toString())),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final counter = context.read<Counter>();
+              counter.increment();
+            },
+            child: Text("Increment"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// class MyCounter extends StateProvider {}
+// class CounterStateNotifier extends StateNotifier<Counter> {
+//   CounterStateNotifier([Counter? counter]) : super(counter ?? Counter(0));
+
+//   void increment() {
+//     state = Counter(state.count + 1);
+//   }
+// }
